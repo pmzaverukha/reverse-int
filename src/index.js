685a029d2613e0ca7c82062d5833c134ef6150ea
Igor Zaverukha <pm.zaverukha@gmail.com>
2021-03-12 22:45:54 +0200

    feat: add body of function reverse

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
 module.exports = function reverse (n) {
-  
+    if (n < 0) {
+        let array = (n*(-1)).toString().split("").reverse().join("");
+        let reverseIntg = 1*array;
+        return(reverseIntg);
+    } else {
+        let array = n.toString().split("").reverse().join("");
+        let reverseIntg = 1*array;
+        return(reverseIntg);
+    }
 }
